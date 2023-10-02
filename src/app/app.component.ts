81fa8fe7718159d01fe5e7339bb624e0e9201abb
Viktor Honchar <viktor.honchar@macbook.hm>
2023-10-02 10:38:01 -0400

    -a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList("Group 13: Ashley, Max, Alan and Viktor", [
+    new TodoItem("Go for run"),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+    new TodoItem("Study for JavaScript", true),
+    new TodoItem("Walk the dog")
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => this.showComplete || !item.complete);
+  }
+
+  addItem(newItem: string) {
+    if (newItem != "") {
+      this.list.addItem(newItem);
+      this.items;
+    }
+  }
+
+  showComplete: boolean = true; //I set it as true to show all tasks on page load
 }
